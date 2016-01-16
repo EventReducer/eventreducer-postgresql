24f70464861a1d089949f463ff1e2e98fdc903ad
Yurii Rashkovskii <yrashk@gmail.com>
2016-01-15 22:27:22 -0800

    Move ObjectMapper initialization to eventreducer-json

--- src/main/java/org/eventreducer/postgresql/PostgreSQLJournal.java
+++ src/main/java/org/eventreducer/postgresql/PostgreSQLJournal.java
@@ -1,20 +1,13 @@
 package org.eventreducer.postgresql;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.eventreducer.Command;
 import org.eventreducer.Event;
 import org.eventreducer.IndexFactory;
 import org.eventreducer.Journal;
 import org.eventreducer.hlc.PhysicalTimeProvider;
-import org.eventreducer.json.EventReducerModule;
+import org.eventreducer.json.ObjectMapper;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
@@ -23,6 +16,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
 
+@Slf4j
 public class PostgreSQLJournal extends Journal {
 
 
@@ -41,47 +35,6 @@ public class PostgreSQLJournal extends Journal {
         flyway.migrate();
         this.dataSource = dataSource;
         this.objectMapper = new ObjectMapper();
-        this.objectMapper.registerModule(new EventReducerModule());
-
-        // mapTyper code taken from Redisson
-        TypeResolverBuilder<?> mapTyper = new ObjectMapper.DefaultTypeResolverBuilder(ObjectMapper.DefaultTyping.NON_FINAL) {
-            public boolean useForType(JavaType t)
-            {
-                switch (_appliesFor) {
-                    case NON_CONCRETE_AND_ARRAYS:
-                        while (t.isArrayType()) {
-                            t = t.getContentType();
-                        }
-                        // fall through
-                    case OBJECT_AND_NON_CONCRETE:
-                        return (t.getRawClass() == Object.class) || !t.isConcrete();
-                    case NON_FINAL:
-                        while (t.isArrayType()) {
-                            t = t.getContentType();
-                        }
-                        // to fix problem with wrong long to int conversion
-                        if (t.getRawClass() == Long.class) {
-                            return true;
-                        }
-                        return !t.isFinal(); // includes Object.class
-                    default:
-                        //case JAVA_LANG_OBJECT:
-                        return (t.getRawClass() == Object.class);
-                }
-            }
-        };
-        mapTyper.init(JsonTypeInfo.Id.CLASS, null);
-        mapTyper.inclusion(JsonTypeInfo.As.PROPERTY);
-        objectMapper.setDefaultTyping(mapTyper);
-
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setVisibilityChecker(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-                .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-                .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
     }
 
     private void checkVersion(DataSource dataSource) throws Exception {
